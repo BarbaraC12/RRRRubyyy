52e342f403a8e229801c3659d9bd6d021aa968e7
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2019-10-03 18:27:41 +0200

    Add files via upload

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,11 +1,11 @@
-def signup #return
-	puts "Choose one password:"
+def signup 
+	puts "Choose one password please :"
 	print ">"
 	password = gets.chomp
 	return password
 end
 
-def login #while
+def login 
 	puts "Welcome! Tape your password:"
 	log = gets.chomp
 	return log
@@ -17,7 +17,7 @@ def welcome_screen (log, password)
 		log = gets.chomp
 	end
 	if log == password
-		puts "Welcome User_968 !"
+		puts "Welcome Master, in your secret zone !!!!"
 	end
 end
 
